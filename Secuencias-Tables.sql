043de827731202fec86be8ad03b47d77135c73ec
Daniel Carmona Jaramillo <daniel.carmona.jaramillo@gmail.com>
2018-11-12 10:18:21 -0500

    Actualizando diagrama ER y actualizando Scripts de creación de tablas

--- Secuencias-Tables.sql
+++ Secuencias-Tables.sql
@@ -1,134 +1,155 @@
-CREATE TABLE countries
+CREATE TABLE Country
 (
   country_id INT PRIMARY KEY,
-  country_coin VARCHAR2(250) NOT NULL,
-  country_description VARCHAR2(250)NOT NULL
+  country_Coin NUMBER NOT NULL,
+  country_description VARCHAR2(30)NOT NULL
 );
 
-CREATE TABLE cities
+CREATE TABLE City
 (
   city_id INT PRIMARY KEY,
   city_description VARCHAR2(250)NOT NULL,
   country_id NOT NULL, 
-  CONSTRAINT fk_city_country FOREIGN KEY(country_id) REFERENCES countries(country_id)
+  base NUMBER NOT NULL, 
+  rate_km NUMBER NOT NULL, 
+  rate_mn NUMBER NOT NULL, 
+  CONSTRAINT fk_city_country FOREIGN KEY(country_id) REFERENCES Country(country_id)
 );
 
-CREATE TABLE lenguages
+CREATE TABLE Language
 (
-  lenguage_id INT PRIMARY KEY,
-  lenguage_description VARCHAR2(250)NOT NULL
+  language_id INT PRIMARY KEY,
+  language_description VARCHAR2(250)NOT NULL
 );
 
-CREATE TABLE profiles
+CREATE TABLE Users
 (
-  profile_id INT PRIMARY KEY,
-  profile_login VARCHAR2(250) NOT NULL,
-  profile_password VARCHAR2(250)NOT NULL,
-  profile_fullname VARCHAR2(250)NOT NULL,
-  profile_protourl VARCHAR2(500)NOT NULL,
-  profile_phone VARCHAR2(250) NOT NULL,
+  user_id INT PRIMARY KEY,
+  user_login VARCHAR2(250) NOT NULL,
+  user_password VARCHAR2(250)NOT NULL,
+  user_fullname VARCHAR2(250)NOT NULL,
+  user_photourl VARCHAR2(500)NOT NULL,
+  user_phone VARCHAR2(250) NOT NULL,
   city_id INT NOT NULL, 
-  lenguage_id INT NOT NULL, 
-  CONSTRAINT fk_profile_city FOREIGN KEY(city_id) REFERENCES cities(city_id),
-  CONSTRAINT fk_profile_lenguage FOREIGN KEY(lenguage_id) REFERENCES lenguages(lenguage_id)
+  language_id INT NOT NULL, 
+  CONSTRAINT fk_profile_city FOREIGN KEY(city_id) REFERENCES City(city_id),
+  CONSTRAINT fk_profile_language FOREIGN KEY(language_id) REFERENCES Language(language_id)
 );
 
-CREATE TABLE drivers
+CREATE TABLE Drivers
 (
   driver_id INT PRIMARY KEY,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_driver_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
+  user_id INT NOT NULL, 
+  CONSTRAINT fk_driver_profile FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE vehicles
+CREATE TABLE Vehicles
 (
   vehicle_id INT PRIMARY KEY,
   vehicle_number VARCHAR2(250) NOT NULL,
   vehicle_brand VARCHAR2(250)NOT NULL,
   vehicle_model VARCHAR2(250)NOT NULL,
-  vehicle_year DATE NOT NULL,
-  driver_id INT NOT NULL, 
-  CONSTRAINT fk_vehicle_driver FOREIGN KEY(driver_id) REFERENCES drivers(driver_id)
+  vehicle_year DATE NOT NULL
 );
 
-CREATE TABLE consignaments
+CREATE TABLE VehiclesDrivers
 (
-  consignament_id INT PRIMARY KEY,
-  consignament_valuegain FLOAT NOT NULL,
-  travel_number INT NOT NULL,
-  consignament_percentuber FLOAT NOT NULL,
-  consignament_totalvalue FLOAT NOT NULL,
-  driver_id INT NOT NULL,
-  CONSTRAINT fk_consignament_driver FOREIGN KEY(driver_id) REFERENCES drivers(driver_id)
+  vdriver_id INT PRIMARY KEY,
+  vehicle_id NOT NULL,
+  driver_id NOT NULL,
+  CONSTRAINT fk_vdriver_vehicle FOREIGN KEY(vehicle_id) REFERENCES Vehicles(vehicle_id),
+  CONSTRAINT fk_vdriver_driver FOREIGN KEY(driver_id) REFERENCES Drivers(driver_id)
 );
 
-CREATE TABLE customers
+CREATE TABLE Consignments
+(
+  consignments_id INT PRIMARY KEY,
+  consignments_valuegain FLOAT NOT NULL,
+  travel_number INT NOT NULL,
+  consignments_percentuber FLOAT NOT NULL,
+  consignments_totalvalue FLOAT NOT NULL,
+  driver_id INT NOT NULL,
+  CONSTRAINT fk_consignments_driver FOREIGN KEY(driver_id) REFERENCES Drivers(driver_id)
+);
+
+CREATE TABLE Company
+(
+  company_id INT PRIMARY KEY,
+  company_name VARCHAR2(250) NOT NULL,
+  company_nit VARCHAR2(250) NOT NULL
+);
+
+
+CREATE TABLE Customers
 (
   customer_id INT PRIMARY KEY,
   reference_code VARCHAR2(250) NOT NULL,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_customer_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
+  user_id INT NOT NULL, 
+  company_id INT,
+  CONSTRAINT fk_customer_profile FOREIGN KEY(user_id) REFERENCES Users(user_id),
+  CONSTRAINT fk_customer_company FOREIGN KEY(company_id) REFERENCES Company(company_id)
 );
 
-CREATE TABLE emails
+CREATE TABLE Email
 (
   email_id INT PRIMARY KEY,
   email_address VARCHAR2(250) NOT NULL,
   email_notification VARCHAR2(250) NOT NULL,
   payment_number VARCHAR2(250) NOT NULL,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_emails_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
+  user_id INT NOT NULL, 
+  CONSTRAINT fk_Email_profile FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE billscompanies
+CREATE TABLE PaymentMethod
+(
+  payment_id INT PRIMARY KEY,
+  payment_detail VARCHAR2(250)NOT NULL,
+  payment_type VARCHAR2(250)NOT NULL,
+  user_id INT NOT NULL, 
+  CONSTRAINT fk_payment_profile FOREIGN KEY(user_id) REFERENCES Users(user_id)
+);
+
+
+CREATE TABLE BillsCompany
 (
   bill_id INT PRIMARY KEY,
   bill_number VARCHAR2(250)NOT NULL,
-  bill_nit INT NOT NULL,
-  bill_descrption VARCHAR2(250) NOT NULL,
+  bill_description VARCHAR2(250)NOT NULL,
   payment_id INT NOT NULL,
-  payment_number INT NOT NULL,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_billscompanies_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
+  CONSTRAINT fk_BillsCompany_profile FOREIGN KEY(payment_id) REFERENCES PaymentMethod(payment_id)
 );
 
-CREATE TABLE serviceTypes
+CREATE TABLE ServiceType
 (
   type_id INT PRIMARY KEY,
   type_description VARCHAR2(250)NOT NULL
 );
 
-CREATE TABLE invitations
+CREATE TABLE Invitations
 (
   invitation_id INT PRIMARY KEY,
   invitation_code INT NOT NULL,
   invitation_status VARCHAR2(250) NOT NULL,
   customer_id INT NOT NULL, 
-  CONSTRAINT fk_invitation_customer FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+  CONSTRAINT fk_invitation_customer FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE paymentMethods
-(
-  payment_id INT PRIMARY KEY,
-  payment_description VARCHAR2(250)NOT NULL,
-  payment_number INT NOT NULL,
-  payment_date DATE NOT NULL,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_payment_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
-);
 
-CREATE TABLE tripsDetail
+
+CREATE TABLE TripDetail
 (
   trip_id INT PRIMARY KEY,
   trip_datetimestart TIMESTAMP NOT NULL,
-  trip_kilometer FLOAT NOT NULL,
   trip_second TIMESTAMP NOT NULL,
   trip_latitude FLOAT NOT NULL,
   trip_logitude FLOAT NOT NULL,
   trip_datetimeend TIMESTAMP NOT NULL
 );
 
-CREATE TABLE travelDetails
+
+
+
+CREATE TABLE TravelDetail
 (
   detail_id INT PRIMARY KEY,
   trail_total INT NOT NULL,
@@ -138,11 +159,12 @@ CREATE TABLE travelDetails
   invitation_code INT,
   vehicle_number VARCHAR2(250) NOT NULL,
   travel_timeend TIMESTAMP NOT NULL,
-  payment_number INT NOT NULL,
-  bill_number VARCHAR2(250)
+  payment_id INT NOT NULL,
+  bill_id INT,
+  CONSTRAINT fk_TravelDetail_payment FOREIGN KEY(payment_id) REFERENCES PaymentMethod(payment_id)
 );
 
-CREATE TABLE travels
+CREATE TABLE Travels
 (
   travel_id INT PRIMARY KEY,
   payment_description VARCHAR2(250)NOT NULL,
@@ -156,24 +178,24 @@ CREATE TABLE travels
   detail_id INT NOT NULL,
   trip_id INT NOT NULL,
   type_id INT NOT NULL,
-  driver_id INT NOT NULL,
+  vdriver_id INT NOT NULL,
   customer_id INT NOT NULL,
-  profile_id INT NOT NULL, 
-  CONSTRAINT fk_travels_driver FOREIGN KEY(driver_id) REFERENCES drivers(driver_id),
-  CONSTRAINT fk_travels_detail FOREIGN KEY(detail_id) REFERENCES travelDetails(detail_id),
-  CONSTRAINT fk_travel_trip FOREIGN KEY(trip_id) REFERENCES tripsDetail(trip_id),
-  CONSTRAINT fk_travels_type FOREIGN KEY(type_id) REFERENCES serviceTypes(type_id),
-  CONSTRAINT fk_travels_customer FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
-  CONSTRAINT fk_paymentmethod_profile FOREIGN KEY(profile_id) REFERENCES profiles(profile_id)
+  user_id INT NOT NULL, 
+  CONSTRAINT fk_Travels_driver FOREIGN KEY(vdriver_id) REFERENCES VehiclesDrivers(vdriver_id),
+  CONSTRAINT fk_Travels_detail FOREIGN KEY(detail_id) REFERENCES TravelDetail(detail_id),
+  CONSTRAINT fk_travel_trip FOREIGN KEY(trip_id) REFERENCES TripDetail(trip_id),
+  CONSTRAINT fk_Travels_type FOREIGN KEY(type_id) REFERENCES ServiceType(type_id),
+  CONSTRAINT fk_Travels_customer FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+  CONSTRAINT fk_paymentmethod_profile FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE travelsShared
+CREATE TABLE TravelShared
 (
   shared_id INT PRIMARY KEY,
   shared_value FLOAT NOT NULL,
   shared_custumer INT NOT NULL,
   detail_id  INT NOT NULL,
-  CONSTRAINT fk_travel_travelShared FOREIGN KEY(detail_id) REFERENCES travelDetails(detail_id)
+  CONSTRAINT fk_travel_travelShared FOREIGN KEY(detail_id) REFERENCES TravelDetail(detail_id)
 );
 
 ---------------------------------------------------------------
@@ -213,7 +235,7 @@ START WITH 1
 INCREMENT BY 1
 NOCYCLE;
 
-CREATE SEQUENCE lenguage_seq
+CREATE SEQUENCE language_seq
 START WITH 1
 INCREMENT BY 1
 NOCYCLE;
@@ -228,7 +250,7 @@ START WITH 1
 INCREMENT BY 1
 NOCYCLE;
 
-CREATE SEQUENCE profiles_seq
+CREATE SEQUENCE users_seq
 START WITH 1
 INCREMENT BY 1
 NOCYCLE;

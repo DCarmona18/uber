2714456716942951ea0f0a0b0dd8626c8e3a4bf2
ANGELA <antoolsystem@gmail.com>
2018-11-17 23:41:40 -0500

    Commir procedimiento y vista

--- newquery.sql
+++ newquery.sql
@@ -25,6 +25,7 @@ CREATE OR REPLACE VIEW  VIAJES_CLIENTES AS
        ,T.travel_totalvalue AS VALOR_TOTAL
        ,CASE WHEN TD.detail_valuedinamic =0 THEN 'FALSO' ELSE 'TRUE' END TARIFA_DINAMICA
        ,S.type_description AS TIPO_SERVICIO
+       ,Y.CITY_DESCRIPTION  AS CIUDAD_VIAJE
   FROM Travels T
   INNER JOIN VehiclesDrivers VD ON VD.vdriver_id = T.vdriver_id
   INNER JOIN Drivers D ON D.driver_id=VD.driver_id 
@@ -33,7 +34,8 @@ CREATE OR REPLACE VIEW  VIAJES_CLIENTES AS
   INNER JOIN Customers C ON C.customer_id=T.customer_id
   INNER JOIN Users U2 ON U2.user_id=C.user_id 
   INNER JOIN TravelDetail TD ON TD.detail_id=T.detail_id
-  INNER JOIN ServiceType S ON S.type_id=T.type_id;
+  INNER JOIN ServiceType S ON S.type_id=T.type_id
+  INNER JOIN City Y ON Y.city_id=T.city_id;
 
 
 --CREACION FUNCION KILOMETROS
@@ -98,30 +100,66 @@ AS
   rate_mn CITY.RATE_MN%TYPE;
   city CITY.city_description%TYPE;
   trip_second TRIPDETAIL.TRIP_SECOND%TYPE;
+  sum_detail NUMBER :=0;
   minutos NUMBER := 0;
   valor_distancia NUMBER:= 0;
   valor_tiempo NUMBER:= 0;
+  VALOR NUMBER := 0;
   CURSOR c_travels IS SELECT * FROM TRAVELS
 BEGIN
   -- Sentencias
-  SELECT T.travel_status, T.city_id, T.travel_timestart, T.travel_timeend, TD.TRIP_SECOND INTO status, city_id, timestart, timeend, trip_second FROM Travels T
+    --e. Deberá buscar todos los detalles de cada viaje y sumarlos. 
+  SELECT T.travel_status
+         ,T.city_id
+         ,T.travel_timestart
+         ,T.travel_timeend
+         ,TD.TRIP_SECOND 
+         ,sum(TV.DETAIL_VALUEEND+TV.DETAIL_VALUEDINAMIC/*+TV.IVA+TV.DESCUENTO*/) AS sum_detail 
+  INTO   status
+         ,city_id
+         ,timestart
+         ,timeend 
+         ,trip_second 
+         ,sum_detail
+  FROM Travels T
   INNER JOIN TripDetail TD ON TD.TRIP_ID = T.TRIP_ID
+  INNER JOIN TravelDetail TV ON TV.detail_id = T.detail_id
   WHERE travel_id = ID_VIAJE;
+  
   --a. Si el estado del viaje es diferente a REALIZADO, deberá insertar 0 en el valor de la tarifa. 
   IF status = 'REALIZADO' THEN
-    UPDATE Travels SET TRAVEL_TOTALVALUE = 0 WHERE travel_id = ID_VIAJE;
+    UPDATE Travels SET TRAVEL_TOTALVALUE = 0 
+    WHERE travel_id = ID_VIAJE;
   END IF;
+  
   --b. Buscar el valor de la tarifa base dependiendo de la ciudad donde se haya hecho el servicio. 
-  SELECT city_description, base, rate_km, rate_mn INTO city,base, rate_km, rate_mn FROM City WHERE city_id = city_id;
+  SELECT city_description
+         ,base
+         ,rate_km
+         ,rate_mn 
+  INTO   city
+         ,base
+         ,rate_km
+         ,rate_mn 
+  FROM City 
+  WHERE city_id = city_id;
   --Se envian parametros  a las funciones y se almacena el resultado en variables declaradas
   --c. Invocar la función VALOR_DISTANCIA 
   valor_distancia := VALOR_DISTANCIA(trail,city);
   --d. Invocar la función VALOR_TIEMPO 
-  valor_tiempo := VALOR_TIEMPO(trip_second,city);
-  --Este se realiza en el query principal que se almacena en la tabla temporal con un calculo de funciones de agregacion
-  --e. Deberá buscar todos los detalles de cada viaje y sumarlos. 
   
+  valor_tiempo := VALOR_TIEMPO(trip_second,city);
+  --CONTROLAR ERRORES EN RETORNOS DEJAR POR DEFECTO VALOR CERO 
+  
+  --Este se realiza en el query principal que se almacena en la tabla temporal con un calculo de funciones de agregacion
   --f. Sumar la tarifa base más el resultado de la función VALOR_DISTANCIA más el resultado de la función VALOR_TIEMPO 
-  --y el resultado de la sumatoria de los detalles del viaje. g. Actualizar el registro del viaje con el resultado obtenido. h. Si alguna de las funciones levanta una excepción, esta deberá ser controlada y actualizar el               valor del viaje con 0.
+  VALOR := (valor_distancia+valor_tiempo+sum_detail+base);
+  --y el resultado de la sumatoria de los detalles del viaje. 
+  --g. Actualizar el registro del viaje con el resultado obtenido. 
+    IF VALOR > 0 THEN
+      UPDATE Travels SET TRAVEL_TOTALVALUE = VALOR
+      WHERE travel_id = ID_VIAJE;
+    END IF;
+  --h. Si alguna de las funciones levanta una excepción, esta deberá ser controlada y actualizar el valor del viaje con 0.
 
 END; 
